4a3aa424fdcd440c5ea2f6cd77161b0bf40549c2
NewtonZhan <zhanyunze0601@gmail.com>
2026-06-27 10:07:41 +0800

    feat: tidy

--- recursivesplitter/recursivesplitter.go
+++ recursivesplitter/recursivesplitter.go
@@ -1,5 +1,9 @@
 package recursivesplitter
 
+import (
+	"context"
+)
+
 // RecursiveSplitter splits the text according to the natural order of the language.
 type RecursiveSplitter struct {
 	ChunkSize  int
@@ -18,4 +22,34 @@ func NewRecursiveSplitter(
 		Overlap:    overlap,
 		Separators: separators,
 	}
+}
+
+// recursiveSplit splits single document using recursive splitting method for each step
+func (splitter *RecursiveSplitter) recursiveSplit(
+	document string,
+	sepIdx int,
+) (
+	[]string,
+	error,
+) {
+	var result []string = []string{}
+	runedDocument := []rune(document)
+	// Directly return the document if the length of the document is smaller than ChunkSize
+	if len(runedDocument) <= splitter.ChunkSize {
+		result = append(result, document)
+		return result, nil
+	}
+	return result, nil
+}
+
+// SplitText splits single document using recursive splitting method
+func (splitter *RecursiveSplitter) SplitText(
+	ctx context.Context,
+	document string,
+) (
+	[]string,
+	error,
+) {
+	result, err := splitter.recursiveSplit(document, 0)
+	return result, err
 }
