fdc5e96ab57e5a297bc00299f172d342404d6348
NewtonZhan <zhanyunze0601@gmail.com>
2026-07-17 10:52:36 +0800

    fix-2

--- internal/embedding/embedding_test.go
+++ internal/embedding/embedding_test.go
@@ -24,6 +24,7 @@ func TestEmbedding(t *testing.T) {
 	mockServer := httptest.NewServer(
 		http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
 				w.Write(data)
 			},
 		),
